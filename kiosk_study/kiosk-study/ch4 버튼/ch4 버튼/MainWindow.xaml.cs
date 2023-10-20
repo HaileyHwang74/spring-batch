d8d0d097539dfb285311acf5ebcf28a904d6d128
halieyHwang <hellociice@gmail.com>
2023-10-20 17:28:52 +0900

    ch4 버튼

---
+++ kiosk_study/kiosk-study/ch4 버튼/ch4 버튼/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ch4_버튼
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Click(object sender, RoutedEventArgs e)
+        {
+            txtb.Text = "버튼을 클릭했습니다.";
+        }
+    }
+}
